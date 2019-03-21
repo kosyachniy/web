f15272c99204e81870ab9457b71df830aefef21c
kosyachniy <polozhev@mail.ru>
2019-03-21 03:24:09 +0300

    Load image

--- app/__init__.py
+++ app/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+from params import LINK
 
 # import logging
 
--- app/api.py
+++ app/api.py
@@ -1,26 +1,29 @@
 from flask import request, jsonify, make_response, current_app
-from app import app
+from app import app, LINK
 
-# import os
+import os
 # import time
-# import re
+import re
+import requests
+import base64
 # import random
 # import string
-# import requests
-# import base64
 # from hashlib import md5
 
-# from mongodb import db
+# # MongoDB
 
-# Socket.IO
+# from pymongo import MongoClient
+# db = MongoClient()['web']
 
-from threading import Lock
-from flask_socketio import SocketIO, emit
+# # Socket.IO
 
-socketio = SocketIO(app, async_mode=None)
+# from threading import Lock
+# from flask_socketio import SocketIO, emit
 
-thread = None
-thread_lock = Lock()
+# socketio = SocketIO(app, async_mode=None)
+
+# thread = None
+# thread_lock = Lock()
 
 # Междоменные запросы
 
@@ -77,6 +80,89 @@ def crossdomain(origin=None, methods=None, headers=None, max_age=21600,
 		return update_wrapper(wrapped_function, f)
 	return decorator
 
+# Загрузка изображения
+
+def max_image(url):
+	all_files = os.listdir(url)
+	last = max([0] + [int(i) for i in all_files if i.isdigit()])
+	return last + 1
+
+def load_image(data, url, name=None, form='png', type='base64'):
+	# Декодирование Base64
+
+	if type == 'base64':
+		data = base64.b64decode(data)
+
+	# Имя файла
+
+	if name:
+		name = str(name)
+
+		for i in os.listdir(url):
+			if re.search(r'^' + name + '\.', i):
+				os.remove(url + '/' + i)
+	else:
+		name = str(max_image(url))
+	
+	# Запись
+
+	with open('{}/{}.{}'.format(url, name, form), 'wb') as file:
+		file.write(data)
+	
+	#
+
+	return name
+
+def reimg(s):
+	k = 0
+
+	while True:
+		x = re.search(r'<img ', s[k:])
+
+		if x:
+			st = list(x.span())
+			st[1] = st[0] + s[k+st[0]:].index('>')
+			vs = ''
+
+			if 'src="' in s[k+st[0]:k+st[1]]:
+				if re.search(r'image/.*;', s[k+st[0]:k+st[1]]) and 'base64,' in s[k+st[0]:k+st[1]]:
+					start = k + st[0] + s[k+st[0]:].index('base64,') + 7
+					stop = start + s[start:].index('"')
+
+					b64 = s[start:stop]
+					form = re.search(r'image/.*;', s[k+st[0]:start]).group(0)[6:-1]
+					adr = load_image(b64, 'app/static/load', form=form)
+
+					# vs = '<img src="{}static/load/{}.{}">'.format(LINK, adr, form)
+					vs = '<img src="/static/load/{}.{}">'.format(adr, form)
+
+				else:
+					start = k + re.search(r'src=".*', s[k:]).span()[0] + 5
+					stop = start + s[start:].index('"')
+					href = s[start:stop]
+
+					if href[:7] == '/static':
+						href = LINK + href[1:]
+
+					if href[:4] == 'http':
+						b64 = str(base64.b64encode(requests.get(href).content))[2:-1]
+						form = href.split('.')[-1]
+						if 'latex' in form:
+							form = 'png'
+
+						adr = load_image(b64, 'app/static/load', form=form)
+						vs = '<img src="/static/load/{}.{}">'.format(adr, form)
+
+			if vs:
+				s = s[:k+st[0]] + vs + s[k+st[1]+1:]
+				k += st[0] + len(vs)
+			else:
+				k += st[1]
+		else:
+			break
+
+	return s
+
 #
 
 
@@ -86,9 +172,18 @@ def process():
 	x = request.json
 	# timestamp = time.time()
 
-	req = {
-		'error': 0,
-		'request': x,
-	}
+	if x['method'] == 'system.test':
+		res = {
+			'error': 0,
+			'request': x,
+		}
 
-	return jsonify(req)
+		return jsonify(res)
+	
+	elif x['method'] == 'image.load':
+		res = {
+			'error': 0,
+			'cont': reimg(x['cont']),
+		}
+
+		return jsonify(res)
