86be8890f35dac2b56b2363cff9b0c34af71ba68
kosyachniy <polozhev@mail.ru>
2020-01-21 12:45:41 +0300

    Run optimization

--- README.md
+++ README.md
@@ -13,3 +13,29 @@ Android | Front-end | React Native | JavaScript | planned
 <img src="re/img/mongodb.png" height="100" />
 <img src="re/img/bootstrap.png" height="100" />
 <img src="re/img/ckeditor.png" height="100" />
+
+## Start
+### Back-end
+1. Customize files ``` sets.py ``` & ``` keys.py ```
+
+2. Virtual environment
+```
+python3 -m venv env
+env/bin/pip install -r requirements.txt
+```
+
+3. Run
+```
+env/bin/gunicorn app:app -c run.py
+```
+
+### Front-end
+1. Virtual environment
+```
+npm install
+```
+
+2. Run
+```
+npm start
+```
--- api/run.py
+++ api/run.py
@@ -1,13 +1,17 @@
-#!env/bin/python
-from app import app
-
 from sets import SERVER
 
 
-if __name__ == '__main__':
-	app.run(
-		host=SERVER['ip'],
-		port=SERVER['port'],
-		debug=True,
-		threaded=True,
-	)
+worker_class = 'eventlet'
+workers = 1
+# threads = 3
+bind = '{}:{}'.format(SERVER['ip'], SERVER['port'])
+# keepalive = 120
+# timeout = 120
+reload = True
+# accesslog = "-" # STDOUT
+# access_log_format = '%(h)s %(l)s %(u)s %(t)s "%(r)s" %(s)s %(b)s "%(q)s" "%(D)s"'
+# pidfile = '/tmp/gunicorn.pid'
+# debug = True
+# loglevel = 'debug'
+# errorlog = '/tmp/gunicorn.log'
+# daemon = True
