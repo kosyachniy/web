7caad277a8d3b6176cfe87c7ad18dd1712a6ac67
kosyachniy <polozhev@mail.ru>
2020-04-28 18:12:35 +0300

    MongoDB start data & simplify

--- api/func/mongodb.py
+++ api/func/mongodb.py
@@ -3,30 +3,12 @@ import json
 from pymongo import MongoClient
 
 
-try:
-	with open('keys.json', 'r') as file:
-		DB = json.loads(file.read())['mongodb']
+with open('keys.json', 'r') as file:
+	MONGO = json.loads(file.read())['mongo']
 
-except:
-	db = MongoClient()['uple']
-
-else:
-	db = MongoClient(
-		username=DB['login'],
-		password=DB['password'],
-		authSource='admin',
-		authMechanism='SCRAM-SHA-1'
-	)[DB['name']]
-
-
-check = db['posts'].find_one({}, {'_id': True})
-
-if not check:
-	post = {
-		'id': 1,
-		'name': 'Тест',
-		'cont': 'Содержание..',
-		'tags': ['Маркетинг',],
-	}
-
-	db['posts'].insert_one(post)
+db = MongoClient(
+	username=MONGO['login'],
+	password=MONGO['password'],
+	authSource='admin',
+	authMechanism='SCRAM-SHA-1'
+)[MONGO['db']]
