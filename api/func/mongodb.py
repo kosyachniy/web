0832a0b19dc69231a1332f645f2d619d37086b21
kosyachniy <polozhev@mail.ru>
2020-05-03 17:26:32 +0300

    Docker worked on Mac

--- api/func/mongodb.py
+++ api/func/mongodb.py
@@ -2,11 +2,15 @@ import json
 
 from pymongo import MongoClient
 
+from sets import HOST
+
 
 with open('keys.json', 'r') as file:
 	MONGO = json.loads(file.read())['mongo']
 
 db = MongoClient(
+	host=HOST,
+	port=27017,
 	username=MONGO['login'],
 	password=MONGO['password'],
 	authSource='admin',
--- api/sets.py
+++ api/sets.py
@@ -1,20 +1,18 @@
+HOST = '127.0.0.1'
+
 SERVER = {
 	'link': 'http://0.0.0.0:5000/',
-	# 'link': 'https://kosyachniy.com/api/',
 	'ip': '0.0.0.0',
 	'port': 5000,
 }
 
 CLIENT = {
 	'link': 'http://0.0.0.0:3000/',
-	# 'link': 'https://kosyachniy.com/',
 	'ip': '0.0.0.0',
 	'port': 3000,
 }
 
 IMAGE = {
-	'link': 'file:///Users/kosyachniy/Re/projects/web/data/load/',
-	'link_opt': 'file:///Users/kosyachniy/Re/projects/web/data/load/opt/',
-	# 'link': 'https://kosyachniy.com/load/',
-	# 'link_opt': 'https://kosyachniy.com/load/opt/',
+	'link': 'http://localhost/load/',
+	'link_opt': 'http://localhost/load/opt/',
 }
