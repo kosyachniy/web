954194eee8c36a30a3c2cbb5e9a63c10aca5e282
kosyachniy <polozhev@mail.ru>
2020-05-03 19:11:14 +0300

    Docker: production version

---
+++ api/sets.prod.py
@@ -0,0 +1,22 @@
+HOST = '127.0.0.1'
+
+SERVER = {
+	'link': 'http://kosyachniy.com:5000/',
+	# 'link': 'https://kosyachniy.com/api/',
+	'ip': '0.0.0.0',
+	'port': 5000,
+}
+
+CLIENT = {
+	'link': 'http://kosyachniy.com/',
+	# 'link': 'https://kosyachniy.com/',
+	'ip': '0.0.0.0',
+	'port': 3000,
+}
+
+IMAGE = {
+	'link': 'http://kosyachniy.com/load/',
+	'link_opt': 'http://kosyachniy.com/load/opt/',
+	# 'link': 'https://kosyachniy.com/load/',
+	# 'link_opt': 'https://kosyachniy.com/load/opt/',
+}
---
+++ web/src/sets.prod.js
@@ -0,0 +1,33 @@
+export const server = {
+	link: 'http://kosyachniy.com:5000/',
+	// link: 'https://kosyachniy.com/api/',
+	ip: '0.0.0.0',
+	port: 5000,
+}
+export const client = {
+	link: 'http://kosyachniy.com/',
+	// link: 'https://kosyachniy.com/',
+	ip: '0.0.0.0',
+	port: 3000,
+}
+export const socket = {
+	link: 'http://kosyachniy.com:5000/',
+	// link: 'https://kosyachniy.com/',
+}
+
+export const name = 'Quateo'
+export const mail = 'polozhev@mail.ru'
+export const phone = '+7 (981) 163-55-78'
+export const social = [{
+	'name': 'vk',
+	'cont': 'https://vk.com/freakiller',
+}, {
+	'name': 'fb',
+	'cont': 'https://www.facebook.com/kosyachniy',
+}, {
+	'name': 'in',
+	'cont': 'https://www.linkedin.com/in/polozalex/',
+}, {
+	'name': 'ig',
+	'cont': 'https://instagram.com/mr.poloz/',
+}]
