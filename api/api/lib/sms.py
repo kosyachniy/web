d7c4732dfe09e0c34c19c3687bc7e0c46320ecaf
kosyachniy <polozhev@mail.ru>
2021-10-01 02:24:23 +0300

    Minor

--- api/api/lib/sms.py
+++ api/api/lib/sms.py
@@ -5,7 +5,7 @@ Sending SMS messages
 import json
 
 
-with open('sets.json', 'r') as file:
+with open('sets.json', 'r', encoding='utf-8') as file:
     sets = json.loads(file.read())['smsc']
     LOGIN = sets['login']
     PASSWORD = sets['password']
