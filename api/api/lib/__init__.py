c38cfe939c312baaad280177515dd5c0b6c207f8
kosyachniy <polozhev@mail.ru>
2021-10-05 12:48:22 +0300

    Move time processing to https://github.com/kosyachniy/lib

--- api/api/lib/__init__.py
+++ api/api/lib/__init__.py
@@ -3,6 +3,5 @@ The main functionality for the API
 """
 
 from ._types import BaseType, validate
-from ._time import get_date
 from ._codes import get_network, get_language
 from ._reports import report
