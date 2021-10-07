d1cd1269f5a7f7fa2461ae93a9be0804a279d8fc
Алексей Полоз <polozhev@mail.ru>
2021-10-08 02:08:09 +0300

    Move type checking to https://github.com/kosyachniy/consys

--- api/api/lib/__init__.py
+++ api/api/lib/__init__.py
@@ -2,11 +2,11 @@
 The main functionality for the API
 """
 
+from consys.types import BaseType, validate
 from libdev.cfg import cfg
 from libdev.codes import get_network, get_language
 from libdev.gen import generate, generate_id, generate_password
 
-from .types import BaseType, validate
 from .reports import report
 
 
