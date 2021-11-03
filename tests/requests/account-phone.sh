cc0044b2b26bfecd7842da605b6727a705f8c9e9
Алексей Полоз <polozhev@mail.ru>
2021-11-04 01:54:37 +0300

    Delete account methods

--- tests/requests/account-phone.sh
+++
@@ -1,8 +0,0 @@
-# Simple
-curl -w "%{time_total}\n" -X POST -H "Content-Type: application/json" -d '{"method": "account.phone", "params": {"phone": "+7 (969) 736-67-30"}, "token": "test"}' http://localhost/api/ https://web.kosyachniy.com/api/
-
-# # Send the code
-# curl -w "%{time_total}\n" -X POST -H "Content-Type: application/json" -d '{"method": "account.phone_send", "params": {"phone": "+7 (969) 736-67-30"}, "token": "token"}' http://localhost/api/ https://web.kosyachniy.com/api/
-
-# # Check the code
-# curl -w "%{time_total}\n" -X POST -H "Content-Type: application/json" -d '{"method": "account.phone_check", "params": {"code": "0556"}, "token": "token"}' http://localhost/api/ https://web.kosyachniy.com/api/
