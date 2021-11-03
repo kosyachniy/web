1cd0d5d85b1b9ce331165b0ba3635dc7173ffd20
Алексей Полоз <polozhev@mail.ru>
2021-11-04 02:14:56 +0300

    Payment methods

---
+++ api/api/models/payment.py
@@ -0,0 +1,19 @@
+"""
+Payment model of User object
+"""
+
+from . import Base, Attribute
+
+
+class Payment(Base):
+    """ Payments data """
+
+    _name = None
+
+    id = Attribute(types=str)
+    type = Attribute(types=str)
+    card = Attribute(types=dict, default={})
+    frequency = Attribute(types=int, default=30)
+    value = Attribute(types=int)
+    currency = Attribute(types=str)
+    discount = Attribute(types=float)
