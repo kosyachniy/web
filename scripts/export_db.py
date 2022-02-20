0d9e7a848dafda15e94026627a0a62e2c6a118cb
Алексей Полоз <polozhev@mail.ru>
2022-02-20 18:01:32 +0300

    Export & import DB

---
+++ scripts/export_db.py
@@ -0,0 +1,25 @@
+import json
+from bson.objectid import ObjectId
+
+from consys._db import get_db
+from libdev.cfg import cfg
+
+
+db = get_db(
+    cfg('mongo.host'),
+    cfg('project_name'),
+    cfg('mongo.login'),
+    cfg('mongo.password'),
+)
+
+
+dbs = [collection['name'] for collection in db.list_collections()]
+
+for db_name in dbs:
+    with open(f'/backup/{db_name}.txt', 'w') as file:
+        for i in db[db_name].find():
+            if isinstance(i['_id'], ObjectId):
+                del i['_id']
+            print(json.dumps(i, ensure_ascii=False), file=file)
+
+    print(f'✅\t{db_name}')
