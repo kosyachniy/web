aa775c524ff8df28abeb8a31c4dc5981e9c53021
kosyachniy <polozhev@mail.ru>
2022-03-12 18:10:20 +0300

    Social auth method

--- api/api/methods/account/social.py
+++ api/api/methods/account/social.py
@@ -2,22 +2,33 @@
 The authorization via social networks method of the account object of the API
 """
 
-# import urllib
-# import base64
+import json
+import urllib
+import base64
 
-# import requests
-# from consys.errors import ErrorAccess, ErrorInvalid, ErrorWrong
+from typing import Union
+import requests
+from libdev.codes import get_network
+from consys.errors import ErrorAccess, ErrorInvalid, ErrorWrong
 
-from api.lib import BaseType, validate # report
-# from api.models.user import User
-# from api.models.token import Token
-# from api.models.action import Action
-# from api.methods.account.online import online_start
+from api.lib import BaseType, validate, cfg, report
+from api.models.user import User
+from api.models.token import Token
+from api.models.track import Track
+from api.methods.account.auth import reg
+from api.methods.account.online import online_start
 
 
 class Type(BaseType):
-    social: str
+    social: Union[str, int]
     code: str
+    # NOTE: For general authorization method fields
+    user: Union[str, int] = None
+    login: str = None
+    avatar: str = None
+    mail: str = None
+    name: str = None
+    surname: str = None
 
 @validate(Type)
 async def handle(request, data):
@@ -28,275 +39,168 @@ async def handle(request, data):
     # TODO: the same token
     # TODO: Сшивать профили, если уже есть с такой почтой / ...
 
-    # fields = {
-    #     'id',
-    #     'login',
-    #     'avatar',
-    #     'name',
-    #     'surname',
-    #     'phone',
-    #     'mail',
-    #     'social',
-    #     'status',
-    # }
+    # Preparing params
+    data.social = get_network(data.social)
 
-    # user_id = 0
-    # new = False
-    # mail = ''
+    fields = {
+        'id',
+        'login',
+        'avatar',
+        'name',
+        'surname',
+        'title',
+        'phone',
+        'mail',
+        'social',
+        'status',
+        # 'subscription',
+        # 'balance',
+    }
 
-    # # ВКонтакте
-    # if data.id == 1:
-    #     link = 'https://oauth.vk.com/access_token?client_id={}' \
-    #            '&client_secret={}&redirect_uri={}callback&code={}'
-    #     response = json.loads(
-    #         requests.get(
-    #             link.format(
-    #                 cfg('vk.id'),
-    #                 cfg('vk.secret'),
-    #                 cfg('client'),
-    #                 data.code,
-    #             )
-    #         ).text
-    #     )
+    # VK
+    if data.social == 3:
+        link = 'https://oauth.vk.com/access_token?client_id={}' \
+               '&client_secret={}&redirect_uri={}callback&code={}'
+        response = json.loads(
+            requests.get(
+                link.format(
+                    cfg('vk.id'),
+                    cfg('vk.secret'),
+                    cfg('client'),
+                    data.code,
+                )
+            ).text
+        )
 
-    #     if 'user_id' in response:
-    #         user_id = response['user_id']
-    #     else:
-    #         raise ErrorAccess('code')
+        if 'user_id' not in response or 'access_token' not in response:
+            raise ErrorAccess('code')
 
-    #     if 'email' in response:
-    #         mail = response['email']
+        data.user = response['user_id']
+        data.mail = response.get('email')
+        token = response['access_token']
 
-    # # Google
-    # elif data.id == 3:
-    #     link = 'https://accounts.google.com/o/oauth2/token'
-    #     cont = {
-    #         'client_id': cfg('google.id'),
-    #         'client_secret': cfg('google.secret'),
-    #         'redirect_uri': f'{cfg('client')}callback',
-    #         'grant_type': 'authorization_code',
-    #         'code': urllib.parse.unquote(data.code),
-    #     }
-    #     response = json.loads(requests.post(link, json=cont).text)
+        # link = 'https://api.vk.com/method/account.getProfileInfo' \
+        #        '?access_token={}&v=5.103'
+        link = 'https://api.vk.com/method/users.get?user_ids={}&fields=' \
+               'photo_max_orig,nickname&access_token={}&v=5.103'
 
-    #     if 'access_token' not in response:
-    #         raise ErrorAccess('code')
+        try:
+            response = json.loads(
+                requests.get(
+                    link.format(data.user, token)
+                ).text
+            )['response'][0]
+        except:
+            raise ErrorAccess('vk')
 
-    #     link = 'https://www.googleapis.com/oauth2/v1/userinfo?access_token={}'
-    #     response = json.loads(
-    #         requests.get(
-    #             link.format(response['access_token'])
-    #         ).text
-    #     )
+        data.name = response.get('first_name')
+        data.surname = response.get('last_name')
+        data.login = response.get('nickname')
+        data.avatar = str(base64.b64encode(
+            requests.get(response['photo_max_orig']).content
+        ))[2:-1] if 'photo_max_orig' in response else None
 
-    #     if 'id' in response:
-    #         user_id = response['id']
-    #     else:
-    #         raise ErrorAccess('code')
+    # Google
+    elif data.social == 4:
+        link = 'https://accounts.google.com/o/oauth2/token'
+        cont = {
+            'client_id': cfg('google.id'),
+            'client_secret': cfg('google.secret'),
+            'redirect_uri': f"{cfg('client')}callback",
+            'grant_type': 'authorization_code',
+            'code': urllib.parse.unquote(data.code),
+        }
+        response = json.loads(requests.post(link, json=cont).text)
 
-    # # Wrong ID
+        if 'access_token' not in response:
+            raise ErrorAccess('code')
 
-    # if not user_id:
-    #     raise ErrorWrong('id')
+        link = 'https://www.googleapis.com/oauth2/v1/userinfo?access_token={}'
+        response = json.loads(
+            requests.get(
+                link.format(response['access_token'])
+            ).text
+        )
 
-    # # Sign in
+        if 'id' not in response:
+            raise ErrorAccess('code')
 
-    # db_condition = {
-    #     'social': {'$elemMatch': {'id': data.id, 'user': user_id}},
-    # }
+        data.user = response['id']
 
-    # db_filter = {
-    #     '_id': False,
-    #     'id': True,
-    #     'login': True,
-    #     'name': True,
-    #     'surname': True,
-    #     'avatar': True,
-    #     'status': True,
-    #     'mail': True,
-    #     # 'rating': True,
-    #     # 'balance': True,
-    # }
+        # link = 'https://www.googleapis.com/oauth2/v1/userinfo' \
+        #        '?access_token={}'.format(data.data['access_token'])
+        # res_google = json.loads(requests.get(link).text)
 
-    # res = db.users.find_one(db_condition, db_filter)
+        data.name = response.get('given_name')
+        data.surname = response.get('family_name')
+        data.mail = response.get('email')
+        data.avatar = str(base64.b64encode(
+            requests.get(response['picture']).content
+        ))[2:-1] if 'picture' in response else None
 
-    # # Wrong password
-    # if not res:
-    #     # Check keys
+    # Wrong ID
+    if not data.user:
+        await report.warning("Wrong ID", {
+            'social': data.social,
+            'social_user': data.user,
+        })
+        raise ErrorWrong('id')
 
-    #     name = ''
-    #     surname = ''
-    #     login = ''
-    #     avatar = None
+    users = User.get(social={'$elemMatch': {
+        'id': data.social,
+        'user': data.user,
+    }}, fields=fields)
 
-    #     if data.id == 1:
-    #         if 'access_token' in response:
-    #             token = response['access_token']
-    #         else:
-    #             raise ErrorAccess('code')
+    if len(users) > 1:
+        await report.warning("More than 1 user", {
+            'social': data.social,
+            'social_user': data.user,
+        })
 
-    #         # link = 'https://api.vk.com/method/account.getProfileInfo' \
-    #         #        '?access_token={}&v=5.103'
-    #         link = 'https://api.vk.com/method/users.get?user_ids={}&fields=' \
-    #                'photo_max_orig,nickname&access_token={}&v=5.103'
+    elif len(users):
+        new = False
+        user = users[0]
 
-    #         try:
-    #             response = json.loads(
-    #                 requests.get(
-    #                     link.format(user_id, token)
-    #                 ).text
-    #             )['response'][0]
-    #         except:
-    #             raise ErrorAccess('vk')
+        # Action tracking
+        Track(
+            title='acc_auth',
+            data={
+                'type': 'social',
+                'social': data.social,
+            },
+            user=user.id,
+        ).save()
 
-    #         try:
-    #             name = response['first_name']
-    #             _check_name(name)
-    #         except Exception:
-    #             name = ''
+    # Register
+    else:
+        new = True
+        user = await reg(request, data, 'social')
 
-    #         try:
-    #             surname = response['last_name']
-    #             _check_surname(surname)
-    #         except Exception:
-    #             surname = ''
+    # Assignment of the token to the user
 
-    #         try:
-    #             login = response['nickname']
-    #             _check_login(login, request.user)
-    #         except Exception:
-    #             login = ''
+    if not request.token:
+        raise ErrorAccess('auth')
 
-    #         try:
-    #             avatar = str(base64.b64encode(
-    #                 requests.get(response['photo_max_orig']).content
-    #             ))[2:-1]
-    #         except Exception:
-    #             avatar = None
+    try:
+        token = Token.get(ids=request.token, fields={'user'})
+    except ErrorWrong:
+        token = Token(id=request.token)
 
-    #         try:
-    #             if mail:
-    #                 _check_mail(mail, request.user)
-    #         except Exception:
-    #             mail = ''
+    if token.user and token.user != user.id:
+        await report.warning("Reauth", {
+            'from': token.user,
+            'to': user.id,
+            'token': request.token,
+        })
 
-    #     elif data.id == 3:
-    #         # link = 'https://www.googleapis.com/oauth2/v1/userinfo' \
-    #         #        '?access_token={}'.format(data.data['access_token'])
-    #         # res_google = json.loads(requests.get(link).text)
+    token.user = user.id
+    token.save()
 
-    #         try:
-    #             name = response['given_name']
-    #             _check_name(name)
-    #         except Exception:
-    #             name = ''
+    # Update online users
+    await online_start(request.sio, request.token)
 
-    #         try:
-    #             surname = response['family_name']
-    #             _check_surname(surname)
-    #         except Exception:
-    #             surname = ''
-
-    #         try:
-    #             mail = response['email']
-    #             _check_mail(mail, request.user)
-    #         except Exception:
-    #             mail = ''
-
-    #         try:
-    #             if response['picture']:
-    #                 avatar = str(base64.b64encode(
-    #                     requests.get(response['picture']).content
-    #                 ))[2:-1]
-    #         except Exception:
-    #             pass
-
-    #     # Sign up
-
-    #     db_condition = {
-    #         'social': {'$elemMatch': {'user': user_id}},
-    #     }
-
-    #     db_filter = {
-    #         '_id': True,
-    #     }
-
-    #     user = db.users.find_one(db_condition, db_filter)
-
-    #     if user:
-    #         raise ErrorWrong('hash')
-
-    #     # Register
-    #     else:
-    #         new = True
-
-    #         action = Action(
-    #             title='acc_reg',
-    #             data={
-    #                 'social': data.social,
-    #                 'ip': request.ip,
-    #                 'social_user': social_user,
-    #                 'social_login': social_login,
-    #                 'social_mail': social_mail,
-    #             },
-    #         )
-
-    #         user = User(
-    #             login=social_login, # TODO: conflicts
-    #             name=social_name,
-    #             surname=social_surname,
-    #             social=[{
-    #                 'id': data.social, # TODO: Several accounts in one network
-    #                 'user': social_user,
-    #                 'login': social_login,
-    #                 'mail': social_mail,
-    #                 'name': social_name,
-    #                 'surname': social_surname,
-    #             }],
-    #             actions=[action.json(default=False)],
-    #             # TODO: without `.json()`
-    #             # TODO: avatar
-    #         )
-
-    #         user.save()
-
-    #         # Report
-    #         await report.important("Registration via social network", {
-    #             'user': user.id,
-    #             'name': f"{social_name or ''} {social_surname or ''}",
-    #             'login': social_login and f"@{social_login}",
-    #             'token': request.token,
-    #             'social': data.social,
-    #         }, tags=['reg'])
-
-    # # Assignment of the token to the user
-
-    # if not request.token:
-    #     raise ErrorInvalid('token')
-
-    # req = {
-    #     'token': request.token,
-    #     'user': user['id'],
-    #     'time': request.timestamp,
-    # }
-    # db.tokens.insert_one(req)
-
-    # # Update online users
-
-    # await online_start(request.sio, request.token)
-
-    # # Response
-
-    # res = {
-    #     'id': user['id'],
-    #     'login': user['login'],
-    #     'name': user['name'],
-    #     'surname': user['surname'],
-    #     'status': user['status'],
-    #     'mail': user['mail'],
-    #     # 'balance': user['balance'],
-    #     # 'rating': user['rating'],
-    #     'new': new,
-    # }
-
-    # return res
+    # Response
+    return {
+        **user.json(fields=fields),
+        'new': new,
+    }
