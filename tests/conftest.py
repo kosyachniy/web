260d04abb906e25f0918440c33e3333fbc7fc993
Alex Poloz <alexypoloz@gmail.com>
2022-11-17 01:08:26 +0700

    Check running MongoDB in GA

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,9 +4,8 @@ import os
 os.chdir(os.getcwd()+'/api/')
 sys.path.append(os.getcwd())
 
-os.environ["PROJECT_NAME"] = 'web'
-os.environ["SERVER"] = 'http://localhost/api/'
-os.environ["CLIENT"] = 'http://localhost/'
-os.environ["MODE"] = 'test'
-os.environ["MONGO_HOST"] = 'localhost:27017'
-os.environ["TG_TOKEN"] = '123456789:AABBCCDDEEFFaabbccddeeff-1234567890'
+os.environ['SERVER'] = 'http://localhost/api/'
+os.environ['CLIENT'] = 'http://localhost/'
+os.environ['MODE'] = 'test'
+os.environ['MONGO_HOST'] = 'mongodb://localhost:27017'
+os.environ['TG_TOKEN'] = '123456789:AABBCCDDEEFFaabbccddeeff-1234567890'
