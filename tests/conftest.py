0aef6fce46dfb5c0720099b7f2f67e0bba37681f
Alex Poloz <alexypoloz@gmail.com>
2022-11-17 01:19:17 +0700

    Fix

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,6 +4,7 @@ import os
 os.chdir(os.getcwd()+'/api/')
 sys.path.append(os.getcwd())
 
+os.environ['PROJECT_NAME'] = 'test'
 os.environ['SERVER'] = 'http://localhost/api/'
 os.environ['CLIENT'] = 'http://localhost/'
 os.environ['MODE'] = 'test'
