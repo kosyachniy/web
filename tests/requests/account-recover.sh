ad8ed186be794220f47cc46b5bebb4a29d6e7574
Alex Poloz <alexypoloz@gmail.com>
2023-01-06 21:41:57 +0700

    Move account requests to Postman

--- tests/requests/account-recover.sh
+++
@@ -1,8 +0,0 @@
-# Login
-curl -w "%{time_total}\n" -X POST -H "Content-Type: application/json" -d '{"method": "account.recover", "params": {"login": "kosyachniy"}, "token": "test"}' http://localhost/api/ https://web.kosyachniy.com/api/
-
-# Mail
-curl -w "%{time_total}\n" -X POST -H "Content-Type: application/json" -d '{"method": "account.recover", "params": {"login": "polozhev@mail.ru"}, "token": "test"}' http://localhost/api/ https://web.kosyachniy.com/api/
-
-# Phone
-curl -w "%{time_total}\n" -X POST -H "Content-Type: application/json" -d '{"method": "account.recover", "params": {"login": "+7 (969) 736-67-30"}, "token": "test"}' http://localhost/api/ https://web.kosyachniy.com/api/
--- tests/requests/account-social.sh
+++
@@ -1,2 +0,0 @@
-# Telegram
-curl -w "%{time_total}\n" -X POST -H "Content-Type: application/json" -d '{"method": "account.social", "params": {"user": 136563129}, "network": "tg"}' http://localhost/api/ https://web.kosyachniy.com/api/
