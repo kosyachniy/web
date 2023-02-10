2941cf1d4907c852172678fd2f6f4f7933f6046a
Alex Poloz <alexypoloz@gmail.com>
2023-02-10 14:00:37 +0700

    Security headers

--- web/pages/_document.jsx
+++ web/pages/_document.jsx
@@ -13,13 +13,16 @@ export default class MyDocument extends Document {
           <link
             rel="stylesheet"
             href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.10.3/font/bootstrap-icons.css"
+            integrity="sha512-c0+vSv9tnGS4fzwTIBFPcdCZ0QwP+aTePvZeAJkYpbj67KvQ5+VrJjDh3lil48LILJxhICQf76dQ8t/BJyOo/g=="
+            crossOrigin="anonymous"
+            referrerPolicy="origin-when-cross-origin"
           />
           <link
             rel="stylesheet"
             href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.2.1/css/all.min.css"
             integrity="sha512-MV7K8+y+gLIBoVD59lQIYicR65iaqukzvf/nwasF0nqhPay5w/9lJmVM2hMDcnK1OnMGCdVK+iQrJ7lzPJQd1w=="
             crossOrigin="anonymous"
-            referrerPolicy="no-referrer"
+            referrerPolicy="origin-when-cross-origin"
           />
         </Head>
         <body>
